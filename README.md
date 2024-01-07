2da079d64ed6d6c23b2229d9b99cd2eadf740513
surendrarathore6342 <154578321+surendrarathore6342@users.noreply.github.com>
2024-01-07 17:07:34 +0530

    array function
    array function Day 1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # php_array__function
 new repository
+
+
+
+is_array
+in_array
+str_replace
+sprintf
+preg_replace
+dirname
+get_class
+class_exists
+
---
+++ array_function.php
@@ -0,0 +1,135 @@
+<?php
+// count
+    // $cars=array("Volvo","BMW","Toyota");
+    // echo count($cars);
+
+// is_array
+    // $a = "Hello";
+    // echo "a is " . is_array($a) . "<br>";
+
+    // $b = array("red", "green", "blue");
+    // echo "b is " . is_array($b) . "<br>";
+
+// substr
+    // echo substr("Hello world",2);
+
+// in_array
+    // $arr = array("red","blue","green","yellow","black");
+    // if(in_array("blue",$arr)){
+    //     echo "Match found";
+    // }
+    // else{
+    //     echo "Match not found";
+    // }
+    
+// Explode
+    // $str = "Hello world. It's a beautiful day.";
+    // print_r (explode(" ",$str));
+
+// Implode
+    // $arr = array('Hello','World!','Beautiful','Day!');
+    // echo implode(" ",$arr)."<br>";
+    // echo implode("+",$arr)."<br>";
+    // echo implode("-",$arr)."<br>"; 
+    // echo implode("X",$arr);
+
+// str_replace
+    // echo str_replace("world","Peter","Hello world!");
+
+// strlen
+        // echo strlen("Hello World");
+
+// array_merge
+    // $a1=array("red","green");
+    // $a2=array("blue","yellow");
+    // print_r(array_merge($a1,$a2));
+
+// strpos
+    // echo strpos("I love php, I love php too","php");
+
+// preg_match
+    // $str = "Visit W3Schools";
+    // $pattern = "/w3schools/i";
+    // echo preg_match($pattern, $str);
+
+// sprintf
+    // $number = 9;
+    // $str = "Beijing";
+    // $txt = sprintf("There are %u million bicycles in %s.",$number,$str);
+    // echo $txt;
+
+
+// trim
+    // $str = "  Hello World!   ";
+    // echo trim($str);
+
+// array_change_key_case
+    // $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+    // print_r(array_change_key_case($age,CASE_UPPER));
+
+    // $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+    // print_r(array_change_key_case($age,CASE_LOWER));
+
+// file_exists
+//     echo file_exists("array_function.php");
+
+
+// preg_replace
+    // $str = 'Visit Microsoft!';
+    // $pattern = '/microsoft/i';
+    // echo preg_replace($pattern, 'W3Schools', $str);
+
+
+// file_get_contents
+    // echo file_get_contents("array_function.php");
+
+
+// array_key_exists
+    // $a=array("Volvo"=>"XC90","BMW"=>"X5");
+    // if (array_key_exists("Volvo",$a))
+    // {
+    // echo "Key exists!";
+    // }
+    // else
+    // {
+    // echo "Key does not exist!";
+    // }
+
+
+// array_keys
+    // $a=array("Volvo"=>"XC90","BMW"=>"X5","Toyota"=>"Highlander");
+    // print_r(array_keys($a));
+
+
+// dirname
+    // echo dirname("c:/testweb/home.php") . "<br />";
+    // echo dirname("c:/testweb/home.php", 2) . "<br />";
+    // echo dirname("/testweb/home.php");
+
+
+
+// function_exists
+    // function WelcomeMsg(){ 
+    //     echo "Welcome to GeeksforGeeks"; 
+    // } 
+    // if (function_exists('WelcomeMsg'))  { 
+    //     echo "WelcomeMsg() function is available.\n"; 
+    // }else{ 
+    //     echo "WelcomeMsg() function is not available.\n"; 
+    // } 
+
+
+// array_map
+    // function myfunction($num)
+    // {
+    //   return($num*$num);
+    // }
+
+    // $a=array(1,2,3,4,5);
+    // print_r(array_map("myfunction",$a));
+
+// array_slice
+    // $a=array("red","green","blue","yellow","brown");
+    // print_r(array_slice($a,3));
+
+    ?>
